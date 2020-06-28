033a0905f66122e419969143f1b4a353fe86708a
JonathanW82 <Jonwheway@live.co.uk>
2020-06-28 18:05:55 +0100

    finished most of the javascript in thegithub js page still need to tidy the html

--- static/js/github.js
+++ static/js/github.js
@@ -1,16 +1,26 @@
-
+/* These Functions start automatically when the page is loaded and sends a request to the 
+gitHub API, requesting all my repos in an array, I then iterate them out with a for loop
+and inject them into the dom with template literals */
 
 $(document).ready(function(getRepos){
     
+    // loading Spinner
+    document.getElementById('overlayBox').innerHTML = `<div id="overlay"><div class="spinner" id="spinner">
+    <iframe onclick="none" src="https://giphy.com/embed/d9QiBcfzg64Io" frameBorder="0" class="pacmanSpinner">
+    </iframe></div><span class="loadingText">LOADING PLEASE WAIT!</span>`;
+
     // Api call to GitHub to pull my repository data
     let api_request = new XMLHttpRequest();
     let endpoint = "https://api.github.com/users";
     let user = "jonathanw82";
     let query = "repos";
 
-    document.getElementById('overlayBox').innerHTML = `<div id="overlay"><div class="spinner" id="spinner">
-    <iframe onclick="none" src="https://giphy.com/embed/d9QiBcfzg64Io" frameBorder="0" class="pacmanSpinner"></iframe></div><span class="loadingText">LOADING PLEASE WAIT!</span>`;
+    // opens the request and sends the parameters
+    api_request.open("GET", endpoint + "/" + user + "/" + query);
+    // starts the request
+    api_request.send();
 
+    // Waiting for the api to return the data
     api_request.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
             let the_response = api_request.responseText;
@@ -19,35 +29,39 @@ $(document).ready(function(getRepos){
             document.getElementById('overlayBox').innerHTML = "";
         }
         else if (this.status == 404){
+            // The message warns us that the server has an error 404
             document.getElementById('overlayBox').innerHTML = "";
-            document.getElementById('warning').innerHTML = `<span><h2>Look's like something went gone wrong try refreshing or come back later!</h2></span>`;
+            document.getElementById('warning').innerHTML = `<span><h2>Look's like something went gone wrong try 
+                                                                    refreshing or come back later!</h2></span>`;
         }
         else if (this.status == 403){
+            // This message warns us we have exceeded out hourly limit of requests 403 to the
+            // github server and display a time we can comeback and try again. 
             var resetTime = new Date(this.getResponseHeader('X-RateLimit-Reset')*1000);
             document.getElementById('overlayBox').innerHTML = "";
-            document.getElementById('warning').innerHTML = `<span><h2>Oops looks like we have upset the server and reached out limits please try again on <br> ${resetTime}</h2></span>`;
+            document.getElementById('warning').innerHTML = `<span><h2>Oops looks like we have reached our limits 
+                                                            please try again on <br> ${resetTime}<br>Sorry for any inconvenience!</h2></span>`;
         }
 
     };
-    api_request.open("GET", endpoint + "/" + user + "/" + query);
-    api_request.send();
 });
 
 function displayGitHubData(github_repo_return){
-   
+   // Total repos are displayed by this hTML injection
     let total_repos = github_repo_return.length;
+    document.getElementById('repoNumbers').innerHTML = `<p>Total number of Repositories to display ${total_repos}</p>`;
     console.log(github_repo_return);
     
-
     for (let i = 0; i < github_repo_return.length; i++){
-
+        // this for loop iterates through the array sent from github and disaplayes the data 
+        // on the HTML page.
        let repoName = github_repo_return[i].name;
        let repoURL = github_repo_return[i].html_url;
        let created = github_repo_return[i].created_at;
        let updated = github_repo_return[i].updated_at;
        let language = github_repo_return[i].language;
 
-       document.getElementById('displayRepos') = ` <div class="col-sm-4"><div class="diaplayRepos"><span><a href="${repoURL}" target="_blank">${repoName}</a><br>${created}<br>${updated}<br>${language}</span></div></div>`
-
+       document.getElementById('displayRepos').innerHTML += `<div class="col-sm-4"><div class="diaplayRepos"><a href="${repoURL}" target="_blank">${repoName}</a><br>${created}
+                                                    <br>${updated}<br>${language}</div></div>`;
     };
 }
